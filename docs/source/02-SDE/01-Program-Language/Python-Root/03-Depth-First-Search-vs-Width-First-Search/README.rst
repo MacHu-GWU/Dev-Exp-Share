58d65c500d0b352a4b551608815a155cd9e7dc8f
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-03-21 10:49:21 -0400

    cmt

---
+++ docs/source/02-SDE/01-Program-Language/Python-Root/03-Depth-First-Search-vs-Width-First-Search/README.rst
@@ -0,0 +1,6 @@
+.. _dfs-vs-wfs-in-python:
+
+Depth First Search vs Width First Search in Python
+==============================================================================
+Keywords: DFS, WFS, 深度优先, 广度优先
+
---
+++ docs/source/02-SDE/01-Program-Language/Python-Root/03-Depth-First-Search-vs-Width-First-Search/dfs_with_wfs.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+::
+
+    t
+    |--- t1
+        |--- t11
+        |--- t12
+    |--- t2
+        |--- t21
+        |--- t22
+
+    # depth first search result
+    t11, t12, t1, t21, t22, t2, t
+
+    # width first search result
+    t, t1, t2, t11, t12, t21, t22
+"""
+from typing import List
+
+
+class Node:
+    def __init__(self, name: str, children: List['Node'] = None):
+        self.name = name
+        if children is None:
+            children = list()
+        self.children = children
+
+    def __repr__(self):
+        return f"'{self.name}'"
+
+
+t11 = Node("t11")
+t12 = Node("t12")
+t21 = Node("t21")
+t22 = Node("t22")
+t1 = Node("t1", [t11, t12])
+t2 = Node("t2", [t21, t22])
+t = Node("t", [t1, t2])
+
+
+def depth_first_search(node: Node):
+    for n in node.children:
+        yield from depth_first_search(n)
+    yield node
+
+
+def width_first_search(node: Node, _is_root=True):
+    if _is_root:
+        yield node
+    for n in node.children:
+        yield n
+    for n in node.children:
+        yield from width_first_search(n, _is_root=False)
+
+
+print(list(depth_first_search(t)))
+print(list(width_first_search(t)))
