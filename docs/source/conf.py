152c0ddb3c2a8155eaa11924e83b95867d69d9fd
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-07-09 17:49:34 -0400

    subprocess

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
     'sphinxcontrib.jinja',
     'sphinx_copybutton',
     'docfly.directives',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
