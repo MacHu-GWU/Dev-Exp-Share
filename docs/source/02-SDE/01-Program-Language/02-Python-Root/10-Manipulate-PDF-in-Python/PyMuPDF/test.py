db57ba11a7d7100c00926e97fd0149026c208663
Sanhe Hu <husanhe@gmail.com>
2022-12-05 15:12:35 -0500

    Update test.py

--- docs/source/02-SDE/01-Program-Language/02-Python-Root/10-Manipulate-PDF-in-Python/PyMuPDF/test.py
+++ docs/source/02-SDE/01-Program-Language/02-Python-Root/10-Manipulate-PDF-in-Python/PyMuPDF/test.py
@@ -1,24 +1,45 @@
 # -*- coding: utf-8 -*-
 
+"""
+Reference:
+
+- https://pypi.org/project/PyMuPDF/
+"""
+
+from io import BytesIO
+import fitz  # This is the PyMuPDF import name
 from pathlib import Path
-import fitz
 
 dir_here = Path(__file__).absolute().parent
+
+# --- Set the sample PDF file path you want to test with
 path_w2_pdf = dir_here.parent / "w2.pdf"
 
-# bytes protocol
-doc = fitz.open(stream=path_w2_pdf.read_bytes())
+path_pdf = path_w2_pdf
+
+# You can use either ``fitz.Document(filename=...)``
+# or use bytes stream ``fitz.Document(stream=...)`` to read the PDF content
+doc = fitz.Document(stream=path_pdf.read_bytes())
+
+# Repair any issues (hopefully) before we hit them
+# See this https://github.com/pymupdf/PyMuPDF/issues/856
+buffer = BytesIO()
+buffer.write(doc.write(clean=True, garbage=4))  # write the document to in-memory buffer
+new_content = buffer.getvalue()
+buffer.close()
+doc = fitz.Document(stream=new_content)
 
 for page_num, page in enumerate(doc, start=1):
-    # split page
-    doc1 = fitz.open()  # new empty PDF
-    doc1.insert_pdf(doc, from_page=page_num-1, to_page=page_num-1)
-    p = dir_here / f"page-{page_num}.pdf"
+    # --- split page
+    doc1 = fitz.Document()  # new empty PDF
+    # doc1.insert_pdf(doc, from_page=page_num-1, to_page=page_num-1)
+    doc1.insert_pdf(doc)
+    p = dir_here / f"{path_pdf.stem}-page-{page_num}.pdf"
     # you cannot write document to io.BytesIO
     doc1.save(f"{p}")
 
-    # convert page to image
-    pix = page.get_pixmap(dpi=300)
-    p = dir_here / f"page-{page_num}.png"
+    # --- convert page to image
+    pix: fitz.Pixmap = page.get_pixmap(dpi=200)
+    p = dir_here / f"{path_pdf.stem}-{page_num}.png"
     # you cannot write pix map to io.BytesIO
-    pix.save(f"{p}")
+    pix.save(f"{p}", output="png")
