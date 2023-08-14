eb2f1de112714c92797638935738a7aa84229673
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-08-14 09:33:53 -0400

    Update example.py

--- docs/source/01-AWS/01-All-AWS-Services-Root/31-Migration-and-Transfer/03-AWS-Transfer-Family-Root/01-Setup-AWS-Managed-SFTP-Server/example.py
+++ docs/source/01-AWS/01-All-AWS-Services-Root/31-Migration-and-Transfer/03-AWS-Transfer-Family-Root/01-Setup-AWS-Managed-SFTP-Server/example.py
@@ -13,6 +13,7 @@ def sftp_connect(
     ssh_private_key: str,
     ssh_password: str,
 ) -> SFTPClient:
+    # 读取 Private Key
     private_key = RSAKey.from_private_key(
         StringIO(ssh_private_key),
         password=ssh_password,
@@ -35,9 +36,9 @@ def sftp_connect(
 if __name__ == "__main__":
     host = "s-1a2b3c4d.server.transfer.us-east-1.amazonaws.com"
     port = 22
-    username = "username"
-    path_pk = Path.home().joinpath(".ssh", "id_rsa")
-    password = "password"
+    username = "username" # 你 SFTP 的用户名
+    path_pk = Path.home().joinpath(".ssh", "id_rsa") # 你的私钥文件
+    password = "password" # 你的 SSH key 的密码 (一般创建 SSH key pair 的时候设置的密码)
     sftp = sftp_connect(
         sftp_hostname=host,
         sftp_port=port,
@@ -46,9 +47,17 @@ if __name__ == "__main__":
         ssh_password=password,
     )
 
+    # 你创建 SFTP server 的时候设置的后台 bucket 和 prefix
     bucket = "your-bucket-name"
+    prefix = ""
+    if prefix.endswith("/"):
+        prefix = prefix[:-1]
+    if prefix:
+        sftp_home_path = f"/{bucket}/{prefix}"
+    else:
+        sftp_home_path = f"/{bucket}"
 
-    sftp.open(f"/{bucket}/{username}/test.txt", "w").write("hello alice")
-    print(sftp.open(f"/{bucket}/{username}/test.txt", "r").read())
-    sftp.open(f"/{bucket}/{username}/test.txt", "w").write("hello bob")
-    print(sftp.open(f"/{bucket}/{username}/test.txt", "r").read())
+    sftp.open(f"{sftp_home_path}/{username}/test.txt", "w").write("hello alice")
+    print(sftp.open(f"{sftp_home_path}/{username}/test.txt", "r").read())
+    sftp.open(f"{sftp_home_path}/{username}/test.txt", "w").write("hello bob")
+    print(sftp.open(f"{sftp_home_path}/{username}/test.txt", "r").read())
